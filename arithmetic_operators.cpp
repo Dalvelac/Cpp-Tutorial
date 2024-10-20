aaec17f77cd82794847293cda4aea2071ea8e319
Daniel Alves Lacambra <dalvelac@myuax.com>
2024-10-20 11:00:40 +0000

    Operaciones aritmeticas

---
+++ arithmetic_operators.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+// Probar los codigos quitando los comentarios de cada uno y ejecutando el programa.
+
+int main(){
+
+    int students = 20; 
+    // Si necesitamos que hayan decimales, cambiamos de "int students" a "float students" o "double students".
+    // int students = (6 - 5) * 2 + 4 / 2; // Se siguen las reglas de la jerarquia de operaciones.
+    
+    //Suma
+    // students = students + 1; // Esto va a sumar 1 a la variable students.
+    // students +=1; // Esto va a sumar 1 a la variable students.
+    // students++; // Esto va a sumar 1 a la variable students, es lo mismo que students +=1, pero se utiliza mas en bucles.
+
+
+    //Resta
+    // students = students - 1; // Esto va a restar 1 a la variable students.
+    // students--; //  Esto va a restar 1 a la variable students, es lo mismo que students -=1, pero se utiliza mas en bucles.
+    // students -=1; // Esto va a restar 1 a la variable students.
+
+
+    //Multiplicacion
+    // students = students * 2; // Esto va a multiplicar por 2 a la variable students.
+    // students *=2; // Esto va a multiplicar por 2 a la variable students.
+
+
+    //Division
+    // students = students / 2; // Esto va a dividir por 2 a la variable students.
+    // students /=2; // Esto va a dividir por 2 a la variable students.
+
+
+    //Resto
+    // students = students % 2; // Esto va a devolver el resto de la division por 2 a la variable students.
+
+    std::cout << students;
+
+}
