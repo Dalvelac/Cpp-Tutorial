c988f4575e38c7e93ad740b8469bef4150dc65b5
Daniel Alves Lacambra <dalvelac@myuax.com>
2024-10-20 17:57:48 +0000

    Solucion

---
+++ Tema 1/Practica/Solucion.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+int main (){
+
+    double a;
+    double b; 
+    double c; 
+
+    std::cout << "Introduce el valor de a: ";
+        std::cin >> a;
+
+    std::cout << "Introduce el valor de b: ";
+        std::cin >> b;
+
+    a = pow(a, 2);
+    b = pow(b, 2);
+
+    c = sqrt(a + b);
+
+    std::cout << "El valor de c es: " << c << std::endl;
+
+    // Se podria simplificar aun mas: c = sqrt(pow(a, 2) + pow(b, 2));
+
+    return 0;
+
+}
